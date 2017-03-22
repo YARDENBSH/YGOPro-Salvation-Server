d518c0f7d0be9cdaa2d5bb7d3cca260a4cb09c78
Zayelion <panthrowzay@gmail.com>
2017-03-22 02:43:32 -0400

    Fusionist

---
+++ scripts/1641882.js
@@ -0,0 +1,19 @@
+/*jslint node : true*/
+'use strict';
+
+// Fusionist
+module.exports = {
+    initial_effect: function initial_effect(card, duel, aux) {
+
+        var effect = {},
+            FUSION_MATERIALS = [
+                38142739, // Petit Angel
+                83464209 // Mystical Sheep #2
+            ];
+        effect = aux.fusionMonster(card, effect, FUSION_MATERIALS);
+
+        card.registerEffect(effect);
+
+        return card;
+    }
+};
